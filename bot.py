f67b99974df0f24f694db2458cd56bed43b92c2c
Muhammadfotima2 <Muhammadfotima24@gmail.com>
2025-07-08 00:28:53 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,31 +1,28 @@
 import telebot
-from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
+from telebot.types import ReplyKeyboardMarkup, KeyboardButton, WebAppInfo
 
 TOKEN = '7861896848:AAHJk1QcelFZ1owB0LO4XXNFflBz-WDZBIE'
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    markup = InlineKeyboardMarkup()
-    webapp_url = 'https://muhammadfotima2.github.io/ekran-webapp/ekran_webapp_index.html'
-    markup.add(
-        InlineKeyboardButton("📱 Открыть каталог EKRAN.TJ", web_app=WebAppInfo(url=webapp_url))
+    markup = ReplyKeyboardMarkup(resize_keyboard=True)
+    webapp_button = KeyboardButton(
+        text='📲 Открыть каталог',
+        web_app=WebAppInfo(url='https://muhammadfotima2.github.io/ekran-webapp/')
     )
+    markup.add(webapp_button)
     bot.send_message(
         message.chat.id,
-        "<b>📦 ХУШ ОМАДЕД БА EKRAN.TJ-KBS</b>\n\n👉 Барои дидани каталог, тугмаро зер кунед:",
+        "📱 Добро пожаловать в магазин <b>EKRAN.TJ-KBS</b>!\n\n"
+        "🛍 Нажмите кнопку ниже, чтобы открыть каталог экранов и выбрать нужную модель и качество:",
         reply_markup=markup,
-        parse_mode="HTML"
+        parse_mode='HTML'
     )
 
 @bot.message_handler(content_types=['web_app_data'])
-def handle_web_app_data(message):
-    data = message.web_app_data.data  # Пример: "Samsung / OLED"
-    model, quality = data.split(' / ')
-    bot.send_message(
-        message.chat.id,
-        f"✅ Шумо интихоб кардед:\n\n📱 Модел: <b>{model}</b>\n💡 Сифат: <b>{quality}</b>",
-        parse_mode="HTML"
-    )
+def handle_web_app(message):
+    data = message.web_app_data.data
+    bot.send_message(message.chat.id, f"✅ Вы выбрали: <b>{data}</b>", parse_mode='HTML')
 
 bot.infinity_polling()
