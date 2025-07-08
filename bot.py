f21856d1978f27c425908866d7232ce6a05e3ad6
Muhammadfotima2 <Muhammadfotima24@gmail.com>
2025-07-08 20:34:04 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,23 @@
 import telebot
-from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+from telebot import types
 
 TOKEN = '7861896848:AAHJk1QcelFZ1owB0LO4XXNFflBz-WDZBIE'
-bot = telebot.TeleBot(TOKEN)
+WEBAPP_URL = 'https://ekran-tj-admin.up.railway.app'  # Адрес WebApp
+
+bot = telebot.TeleBot(TOKEN, parse_mode='HTML')
 
 @bot.message_handler(commands=['start'])
-def welcome(message):
-    markup = InlineKeyboardMarkup()
-    markup.add(InlineKeyboardButton("📦 Каталог", callback_data='catalog'))
-    bot.send_message(message.chat.id, "Добро пожаловать в EKRAN.TJ-KBS!\n\nНажмите кнопку ниже, чтобы открыть каталог и оформить заказ:", reply_markup=markup)
+def send_welcome(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    
+    catalog_button = types.KeyboardButton("📦 Каталог", web_app=types.WebAppInfo(url=WEBAPP_URL))
+    markup.add(catalog_button)
 
-@bot.callback_query_handler(func=lambda call: True)
-def handle_query(call):
-    if call.data == 'catalog':
-        bot.send_message(call.message.chat.id, "🛒 Каталог:\n\n📱 iPhone XR\nКачество: Incell\nЦена: 240 сомонӣ\n\n📱 Samsung A13\nКачество: Original\nЦена: 100 сомонӣ")
+    bot.send_message(
+        message.chat.id,
+        "👋 Добро пожаловать в <b>EKRAN.TJ</b>\n\n"
+        "Нажмите кнопку ниже, чтобы открыть админ-панель:",
+        reply_markup=markup
+    )
 
 bot.polling()
