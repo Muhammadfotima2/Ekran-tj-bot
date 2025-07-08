7f279c3a7b4ff6d3e5eb107dc60f15e114f7b1d7
Muhammadfotima2 <Muhammadfotima24@gmail.com>
2025-07-08 16:06:29 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,35 +4,32 @@ from telebot import types
 TOKEN = '7861896848:AAHJk1QcelFZ1owB0LO4XXNFflBz-WDZBIE'
 bot = telebot.TeleBot(TOKEN, parse_mode='HTML')
 
-WEBAPP_URL = 'https://angelic-gratitude-production.up.railway.app'  # <-- твой WebApp
+WEBAPP_URL = 'https://angelic-gratitude-production.up.railway.app'
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    webapp_btn = types.KeyboardButton(text="🟢 Оғоз кардан", web_app=types.WebAppInfo(url=WEBAPP_URL))
-    markup.add(webapp_btn)
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    web_app = types.WebAppInfo(url=WEBAPP_URL)
+    keyboard.add(types.KeyboardButton("📦 Каталог", web_app=web_app))
 
     bot.send_message(
         message.chat.id,
-        "📦 <b>Хуш омадед ба EKRAN.TJ-KBS!</b>
-"
-        "📱 Мо экранҳо барои iPhone, Samsung, Xiaomi ва дигар моделҳо дорем.
-
-"
-        "👇 Барои дидани каталог, тугмаи «Оғоз кардан»-ро пахш намоед.",
-        reply_markup=markup
+        "📦 Добро пожаловать в <b>EKRAN.TJ-KBS</b>!\n"
+        "Лутфан, тугмаи <b>«Оғоз кардан»</b>-ро пахш намоед, то каталоги мо кушода шавад.",
+        reply_markup=keyboard
     )
 
 @bot.message_handler(content_types=['web_app_data'])
-def handle_webapp_data(message):
+def handle_webapp(message):
     data = message.web_app_data.data
 
-    # Иконки барои бонкҳо
     html_message = data.replace(
         "🏦",
-        "🏦 <img src='https://telegra.ph/file/39f3e25047099ad71f378.png' width='20'/> Alif Bank ё <img src='https://telegra.ph/file/7e611e15399039c9179d0.png' width='20'/> Dushanbe-City"
+        "🏦 <img src='https://telegra.ph/file/39f3e25047099ad71f378.png' width='20'/> Alif Bank "
+        "ё <img src='https://telegra.ph/file/7e611e15399039c9179d0.png' width='20'/> Dushanbe-City"
     )
 
     bot.send_message(message.chat.id, html_message)
 
 bot.polling()
+
