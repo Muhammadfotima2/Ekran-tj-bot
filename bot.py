2bae0f7c158060fbb59209e9c051d1f49c9f23ee
Muhammadfotima2 <Muhammadfotima24@gmail.com>
2025-07-09 01:32:45 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,15 +2,13 @@ import telebot
 from telebot import types
 
 TOKEN = '7861896848:AAHJk1QcelFZ1owB0LO4XXNFflBz-WDZBIE'
-WEBAPP_URL = 'https://web-production-3878b.up.railway.app'
-  # Адрес твоей админ-панели
+WEBAPP_URL = 'https://web-production-3878b.up.railway.app'  # Адрес твоей админ-панели
 
 bot = telebot.TeleBot(TOKEN, parse_mode='HTML')
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    
     admin_button = types.KeyboardButton("🛠 Админ-панель", web_app=types.WebAppInfo(url=WEBAPP_URL))
     markup.add(admin_button)
 
