29ed3709176353e5f62f9331d4db8da9415d82bf
Muhammadfotima2 <Muhammadfotima24@gmail.com>
2025-07-09 16:55:09 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,10 @@
 import telebot
 from telebot import types
 
+# Токен бота
 TOKEN = '7861896848:AAHJk1QcelFZ1owB0LO4XXNFflBz-WDZBIE'
+
+# Адрес WebApp (каталог)
 WEBAPP_URL = 'https://ekran-webapp-production-2297.up.railway.app'
 
 bot = telebot.TeleBot(TOKEN, parse_mode='HTML')
@@ -9,13 +12,15 @@ bot = telebot.TeleBot(TOKEN, parse_mode='HTML')
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    webapp_btn = types.KeyboardButton("🛍 Каталог", web_app=types.WebAppInfo(url=WEBAPP_URL))
-    markup.add(webapp_btn)
+    catalog_btn = types.KeyboardButton("🛍 Каталог", web_app=types.WebAppInfo(url=WEBAPP_URL))
+    markup.add(catalog_btn)
 
     bot.send_message(
         message.chat.id,
-        "👋 Добро пожаловать в <b>EKRAN.TJ</b>\n\nНажмите кнопку ниже, чтобы открыть каталог товаров:",
+        "👋 Добро пожаловать в <b>EKRAN.TJ</b>\n\n"
+        "Нажмите кнопку ниже, чтобы открыть каталог товаров:",
         reply_markup=markup
     )
 
+# Для polling
 bot.infinity_polling()
