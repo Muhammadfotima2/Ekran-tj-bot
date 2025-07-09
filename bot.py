3c6a4fe239fbacbc79fa8df60b34fe649e6e9bcd
Muhammadfotima2 <Muhammadfotima24@gmail.com>
2025-07-09 11:21:21 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,24 @@
 import telebot
+from telebot import types
 
 TOKEN = '7861896848:AAHJk1QcelFZ1owB0LO4XXNFflBz-WDZBIE'
+WEBAPP_URL = 'https://web-production-3878b.up.railway.app'
+
 bot = telebot.TeleBot(TOKEN, parse_mode='HTML')
+
+bot.remove_webhook()  # Чтобы не мешал webhook
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "👋 Привет! Бот работает ✅")
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    webapp_btn = types.KeyboardButton("🛠 Админ-панель", web_app=types.WebAppInfo(url=WEBAPP_URL))
+    markup.add(webapp_btn)
 
-bot.remove_webhook()  # ВАЖНО: отключает Webhook, чтобы polling работал
+    bot.send_message(
+        message.chat.id,
+        "👋 Добро пожаловать в <b>EKRAN.TJ</b>\n\n"
+        "Нажмите кнопку ниже, чтобы открыть <b>админ-панель</b>:",
+        reply_markup=markup
+    )
+
 bot.polling(non_stop=True)
