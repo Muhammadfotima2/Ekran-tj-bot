a8624212bf39f8a7bf1cf65bf4be89b969bf2643
Muhammadfotima2 <Muhammadfotima24@gmail.com>
2025-07-09 11:08:58 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,10 @@
 import telebot
 
 TOKEN = '7861896848:AAHJk1QcelFZ1owB0LO4XXNFflBz-WDZBIE'
-
 bot = telebot.TeleBot(TOKEN, parse_mode='HTML')
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.send_message(message.chat.id, "✅ Бот работает! Добро пожаловать в EKRAN.TJ 😊")
+    bot.reply_to(message, "👋 Привет! Бот работает ✅")
 
-bot.polling()
+bot.polling(non_stop=True)
