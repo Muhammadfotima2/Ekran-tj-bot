c5fdb2441a1278bc703408e9c418eec771f06b0c
Muhammadfotima2 <Muhammadfotima24@gmail.com>
2025-07-09 11:06:11 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,42 +1,11 @@
 import telebot
-from telebot import types
-from flask import Flask, request
 
 TOKEN = '7861896848:AAHJk1QcelFZ1owB0LO4XXNFflBz-WDZBIE'
-WEBAPP_URL = 'https://web-production-3878b.up.railway.app'
-WEBHOOK_URL = 'https://ekran-tj-bot.up.railway.app/'
 
 bot = telebot.TeleBot(TOKEN, parse_mode='HTML')
-app = Flask(__name__)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    admin_button = types.KeyboardButton("🛠 Админ-панель", web_app=types.WebAppInfo(url=WEBAPP_URL))
-    markup.add(admin_button)
+    bot.send_message(message.chat.id, "✅ Бот работает! Добро пожаловать в EKRAN.TJ 😊")
 
-    bot.send_message(
-        message.chat.id,
-        "👋 Добро пожаловать в <b>EKRAN.TJ</b>\n\n"
-        "Нажмите кнопку ниже, чтобы открыть <b>админ-панель</b>:",
-        reply_markup=markup
-    )
-
-@bot.message_handler(content_types=['web_app_data'])
-def handle_webapp_data(message):
-    data = message.web_app_data.data
-    bot.send_message(message.chat.id, f"📥 Получены данные из WebApp:\n{data}")
-
-@app.route('/', methods=['POST'])
-def webhook():
-    if request.headers.get('content-type') == 'application/json':
-        json_str = request.get_data().decode('utf-8')
-        update = telebot.types.Update.de_json(json_str)
-        bot.process_new_updates([update])
-        return '', 200
-    return '', 403
-
-if __name__ == '__main__':
-    bot.remove_webhook()
-    bot.set_webhook(url=WEBHOOK_URL)
-    app.run(host='0.0.0.0', port=8080)
+bot.polling()
