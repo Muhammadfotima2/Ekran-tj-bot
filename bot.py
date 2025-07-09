3f256e80f359d14f22e8ef449ab766b5b15dd5d1
Muhammadfotima2 <Muhammadfotima24@gmail.com>
2025-07-09 21:19:10 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,21 +2,28 @@ import telebot
 from telebot import types
 
 TOKEN = '7861896848:AAHJk1QcelFZ1owB0LO4XXNFflBz-WDZBIE'
-WEBAPP_CATALOG_URL = 'https://ekran-tj-catalog-production.up.railway.app/'  # Вставь адрес каталога
+WEBAPP_CATALOG_URL = 'https://ekran-tj-bot-production-XXXXX.up.railway.app'
+WEBAPP_ADMIN_URL = 'https://ekran-tj-admin-production-XXXXX.up.railway.app/admin'
 
-bot = telebot.TeleBot(TOKEN, parse_mode='HTML')
+ADMIN_ID = 6172156061  # ← замени на свой Telegram ID
+
+bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    catalog_button = types.KeyboardButton("📦 Каталог товаров", web_app=types.WebAppInfo(url=WEBAPP_CATALOG_URL))
+
+    catalog_button = types.KeyboardButton("🛒 Каталог", web_app=types.WebAppInfo(url=WEBAPP_CATALOG_URL))
     markup.add(catalog_button)
+
+    # Показываем кнопку админ-панели только админу
+    if message.from_user.id == ADMIN_ID:
+        admin_button = types.KeyboardButton("🛠 Админ-панель", web_app=types.WebAppInfo(url=WEBAPP_ADMIN_URL))
+        markup.add(admin_button)
 
     bot.send_message(
         message.chat.id,
-        "👋 <b>Хуш омадед ба мағозаи EKRAN.TJ-KBS!</b>\n\n"
-        "📲 Барои дидани <b>каталоги экранҳо</b> кнопкаро пахш кунед:",
-        reply_markup=markup
+        "👋 Добро пожаловать в <b>EKRAN.TJ</b>\n\nВыберите нужный раздел ниже:",
+        reply_markup=markup,
+        parse_mode='HTML'
     )
-
-bot.polling(non_stop=True)
